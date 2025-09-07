7a225ed6c10f5cd5eee57ea996b5a5f25e6f9d58
Frozn11 <karim.elkhawankey@gmail.com>
2025-09-07 03:00:29 +0300

    Update

---
+++ 1.py
@@ -0,0 +1,10 @@
+import random
+
+def main(text):
+    random_number = random.randint(500,5000)
+    text_list = ["KG", "TON"]
+    random_text = random.choice(text_list)
+    print(f"Yo MAMA IS SO FAT SHE WEIGHTS {random_number} {random_text} {text}")
+
+if __name__ == "__main__":
+    main("LUL")
